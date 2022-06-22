c174af2656370112224b34a19b29c47898500633
jorge-25 <jhag25@hotmail.es>
2022-06-22 12:26:58 -0400

    Algo de avance

--- createw_db.py
+++ createw_db.py
@@ -5,8 +5,8 @@ import sys
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
-        user="jp",
-        password="jp",
+        user="jorge",
+        password="jorge25",
         host="localhost",
         port=3306,
 
@@ -18,12 +18,15 @@ except mariadb.Error as e:
 # Get Cursor
 cur = conn.cursor()
 
-# Execute SQL command
+#Execute SQL command
 #cur.execute("CREATE DATABASE Proyectobd")
 cur.execute("USE Proyectobd")
 #cur.execute("CREATE TABLE medioPrensa(idioma VARCHAR(100),region VARCHAR(130),pais VARCHAR(100),nombre VARCHAR(100) PRIMARY KEY,url VARCHAR(250),fechaCreacion DATE )")
 #cur.execute("CREATE TABLE dueño(nombre VARCHAR(100) PRIMARY KEY,personaN BOOL)")
 #cur.execute("CREATE TABLE administrar(fecha DATE,nombreMedio VARCHAR(100) ,nombreDueño VARCHAR(100),FOREIGN KEY (nombreMedio) REFERENCES medioPrensa(nombre),FOREIGN KEY (nombreDueño) REFERENCES dueño(nombre),PRIMARY KEY(fecha,nombreMedio,nombreDueño))")
+#cur.execute("CREATE TABLE noticia(url VARCHAR(100) PRIMARY KEY, nombreMedio VARCHAR(100), fecha DATE, titulo VARCHAR(100), contenido VARCHAR(100), FOREIGN KEY (nombreMedio) REFERENCES medioPrensa(nombre))")
+#cur.execute("CREATE TABLE persona(nombre VARCHAR(100) PRIMARY KEY, fechaNacimiento DATE, profesion VARCHAR(100), nacionalidad VARCHAR(100))")
+cur.execute("CREATE TABLE mencionar(urlNoticia VARCHAR(100), nombrePersona VARCHAR(100), FOREIGN KEY (urlNoticia) REFERENCES noticia(url), FOREIGN KEY (nombrePersona) REFERENCES persona(nombre), PRIMARY KEY(urlNoticia, nombrePersona))")
 
 cur.execute("describe medioPrensa")
 # Show results
