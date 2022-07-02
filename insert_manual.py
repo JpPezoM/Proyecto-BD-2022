7957b60d51382571bfb66e1de1f966b13b9b5147
JpPezoM <juan.pablo.pzm@gmail.com>
2022-07-02 01:22:45 -0400

    Faltan añadir elemntos

--- insert_manual.py
+++ insert_manual.py
@@ -20,4 +20,10 @@ cur = conn.cursor()
 
 cur.execute("USE Proyectobd")
 cur.execute("INSERT INTO medioPrensa VALUES ('español','Arica y parinacota','Chile','aaa','1','aaa','2012/01/28');")
-
+cur.execute("INSERT INTO dueño VALUES ('Jose Joestar',true);")
+cur.execute("INSERT INTO administrar VALUES ('2012/01/28');")
+cur.execute("INSERT INTO noticia VALUES (url VARCHAR(100), nombreMedio VARCHAR(100), '2012/01/28', titulo VARCHAR(100), contenido VARCHAR(100));")
+cur.execute("INSERT INTO persona VALUES ('Sebastian Piñera', '2012/01/28', 'Presidente', 'Chileno');")
+cur.execute("INSERT INTO mencionar VALUES (urlNoticia VARCHAR(100), nombrePersona VARCHAR(100))")
+cur.execute("INSERT INTO popularidad VALUES ('1','2012/01/28' , '20000')")
+cur.execute("INSERT INTO evaluar VALUES (nombrePersona , idPopularidad)")
