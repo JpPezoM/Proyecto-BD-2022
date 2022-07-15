6e712be51797a0eaaf97683fbfcab7f71d728d75
jorge-25 <jhag25@hotmail.es>
2022-07-14 20:28:45 -0400

    Tenemos el 4

--- insert_manual.py
+++ insert_manual.py
@@ -1,12 +1,13 @@
 #pip install mariadb
+from ast import For
 import mariadb
 import sys
 
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
-        user="jp",
-        password="kititi123",
+        user="jorge",
+        password="jorge25",
         host="localhost",
         port=3306,
 
@@ -19,15 +20,41 @@ except mariadb.Error as e:
 cur = conn.cursor()
 
 cur.execute("USE Proyectobd")
+
 cur.execute("INSERT INTO medioPrensa (idioma, region, pais, nombre, url, fechaCreacion) VALUES ('español','Arica y parinacota','Chile','El Morrocotudo','https://www.elmorrocotudo.cl','2005/01/01')")
+cur.execute("INSERT INTO medioPrensa (idioma, region, pais, nombre, url, fechaCreacion) VALUES ('español','Arica y parinacota','Chile','La Estrella de Arica','https://www.estrellaarica.cl','1976/02/08')")
+
 cur.execute("INSERT INTO dueño VALUES ('GrupoMiVoz',false);")
+cur.execute("INSERT INTO dueño VALUES ('Eduardo Campos Correa',true);")
+
 cur.execute("INSERT INTO administrar VALUES ('2005/01/01', 'El Morrocotudo', 'GrupoMiVoz');")
+cur.execute("INSERT INTO administrar VALUES ('2011/02/07', 'La Estrella de Arica', 'Eduardo Campos Correa');")
+
 cur.execute("INSERT INTO noticia VALUES ('0', 'https://www.elmorrocotudo.cl/noticia/economia/repoblamiento-de-sectores-rurales-seremi-de-transportes-de-arica-redisenara-transpo', 'El Morrocotudo', '2022/07/05', 'Repoblamiento de Sectores Rurales: Seremi de Transportes de Arica rediseñará transporte rural', ' Cuando se realizó el CENSO del 2017 la población de la Región alcanzó a las 226.068 personas. De ese total el 97,9% de ellos declaró vivir en la comuna de Arica, 1,2% en la comuna de Putre, 0,6% en la comuna de Camarones y 0,3% en la comuna de General Lagos. ');")
+
 cur.execute("INSERT INTO persona VALUES ('Pablo Maturana', '1992/01/28', 'Seremi de transportes', 'Chileno');")
 cur.execute("INSERT INTO persona VALUES ('Gabriel Boric', '1992/01/28', 'Presidente', 'Chileno');")
+
 cur.execute("INSERT INTO mencionar VALUES ('0', 'Pablo Maturana')")
 cur.execute("INSERT INTO mencionar VALUES ('0', 'Gabriel Boric')")
+
 cur.execute("INSERT INTO popularidad VALUES ('0','2022/07/06' , '0')")
+cur.execute("INSERT INTO popularidad VALUES ('1','2022/07/07' , '1')")
+cur.execute("INSERT INTO popularidad VALUES ('2','2022/07/08' , '2')")
+cur.execute("INSERT INTO popularidad VALUES ('3','2022/07/09' , '3')")
+cur.execute("INSERT INTO popularidad VALUES ('4','2022/07/10' , '4')")
+
+cur.execute("INSERT INTO popularidad VALUES ('5','2022/07/10' , '3')")
+cur.execute("INSERT INTO popularidad VALUES ('6','2022/07/10' , '4')")
+
 cur.execute("INSERT INTO evaluar VALUES ('Pablo Maturana' , '0')")
+cur.execute("INSERT INTO evaluar VALUES ('Pablo Maturana' , '1')")
+cur.execute("INSERT INTO evaluar VALUES ('Pablo Maturana' , '2')")
+cur.execute("INSERT INTO evaluar VALUES ('Pablo Maturana' , '3')")
+cur.execute("INSERT INTO evaluar VALUES ('Pablo Maturana' , '4')")
+
+cur.execute("INSERT INTO evaluar VALUES ('Gabriel Boric' , '5')")
+cur.execute("INSERT INTO evaluar VALUES ('Gabriel Boric' , '6')")
+
 cur.execute("commit;")
 conn.close()
--- requests.py
+++ requests.py
@@ -5,8 +5,8 @@ import sys
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
-        user="jp",
-        password="kititi123",
+        user="jorge",
+        password="jorge25",
         host="localhost",
         port=3306,
 
@@ -21,8 +21,13 @@ cur = conn.cursor()
 cur.execute("USE Proyectobd;")
 #¿Cuántas noticias fueron publicadas por cada medio de prensa?
 cur.execute("SELECT m.nombre, count(*) FROM medioPrensa m JOIN noticia n ON m.nombre=n.nombreMedio GROUP BY n.nombreMedio ORDER BY count(*) DESC")
+#¿Quienes son las personas mencionadas en las noticias de un día específico?
 cur.execute("SELECT m.nombrePersona,n.idNoticia,n.fecha FROM noticia n JOIN mencionar m ON n.idNoticia=m.idNoticia WHERE n.fecha='2022/07/05'")
-cur.execute("SELECT ")
+#¿Cómo evoluciona la popularidad de una persona específica?
+cur.execute("SELECT p.nombre,po.fecha,po.trafico FROM persona p JOIN evaluar e ON p.nombre=e.nombrePersona JOIN popularidad po ON po.idPopularidad=e.idPopularidad WHERE p.nombre='Gabriel Boric' ORDER BY po.fecha DESC")
+#¿Cuáles son los 5 medios de prensa más antiguos en una región especifica?
+cur.execute("SELECT nombre, fechaCreacion FROM medioPrensa WHERE region = 'Arica y parinacota' ORDER BY fechaCreacion ASC LIMIT 5")
+
 
 # Show results
 for row in cur:
